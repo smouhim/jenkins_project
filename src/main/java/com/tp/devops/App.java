f4121e737599fb4f8bf6c91e25eb64c3b1512db1
Mouhim Sanaa <106080625+Mouu123@users.noreply.github.com>
2026-04-09 14:41:47 +0100

    trigger

--- src/main/java/com/tp/devops/App.java
+++ src/main/java/com/tp/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "je viens de changer le code et on va voir est ce que cela va etre détecté automatiquement" );
     }
 }
